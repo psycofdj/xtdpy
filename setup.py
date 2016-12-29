95bbb53a6621c9763a0e903c6f957a11ba497408
Damien Nadé <Anvil@users.noreply.github.com>
2016-12-29 11:30:28 +0100

    Debian metadata update (#32)
    * source package renamed as xtdpy
    
    * renamed bin package as python3-xtd to respect debian stack naming convention
    
    * using python3 instead of python
    
    * Added missing 'requests' dependency
    
    * added missing build-dependency
    
    * using setuptools instead of distutils, added install_requires
    
    * explicit test_suite

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ __author__    = "Xavier MARCELET <xavier@marcelet.com>"
 #------------------------------------------------------------------#
 
 import sys
-from distutils.core import setup
+from setuptools import setup
 sys.path.insert(0, ".")
 import xtd
 
@@ -17,6 +17,8 @@ setup(
               'xtd.core.param',      'xtd.core.stat',   'xtd.core.tools',
               'xtd.core.logger',     'xtd.core.config', 'xtd.network.client',
               'xtd.network.server'],
+  install_requires = ["cherrypy", "termcolor", "pycurl", "requests"],
+  test_suite   = 'xtd.test',
   version      = xtd.__version__,
   description  = xtd.__description__,
   author       = xtd.__author__.split("<")[0].strip(),
